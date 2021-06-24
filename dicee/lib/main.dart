2ccd8b44dfe470eb97a5dd9b99866739b4da08e8
mittalayush1430 <mittalayush1430@gmail.com>
2021-06-24 23:26:32 +0530

    added dice app folder

---
+++ dicee/lib/main.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      backgroundColor: Colors.amber,
+      appBar: AppBar(
+        title: Text('Dicee'),
+        backgroundColor: Colors.black,
+      ),
+      body: DicePage(),
+    ),
+  ));
+}
+
+class DicePage extends StatefulWidget {
+  const DicePage({Key? key}) : super(key: key);
+
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int rightDice = 1;
+  int leftDice = 6;
+
+  void changeDiceFace() {
+    leftDice = Random().nextInt(6) + 1;
+    rightDice = Random().nextInt(6) + 1;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        // crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Expanded(
+              child: TextButton(
+            onPressed: () {
+              setState(() {
+                changeDiceFace();
+              });
+            },
+            child: Image.asset('assets/dice$leftDice.png'),
+          )),
+          Expanded(
+              child: TextButton(
+            onPressed: () {
+              setState(() {
+                changeDiceFace();
+              });
+            },
+            child: Image.asset('assets/dice$rightDice.png'),
+          )),
+        ],
+      ),
+    );
+  }
+}
